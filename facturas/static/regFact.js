dc72b179369c4eaaf018f044215cbe14c3fad57c
Rafael Mercedes <rafaelmersant@gmail.com>
2014-10-01 00:42:54 -0400

    Fue ajustado el diseño de la tabla de productos y fue incorporada la funcion de agregar productos una vez seleccionados

--- facturas/static/regFact.js
+++ facturas/static/regFact.js
@@ -1,54 +1,85 @@
+// SELECCIONAR TEXTO COMPLETO CUANDO ENTRA EL FOCUS
+$("input[type='text']").click(function () {
+   $(this).select();
+});
+
+
+// SECCION DE APARTADOS
 $(document).ready(function() {
 	$('#idApartados').addClass('Apartados');
 })
 
-$('#btAdd').click(function(){
+// CUANDO SEA QUITADO EL FOCUS DE LA BUSQUEDAD DE PRODUCTOS
+$('#iProd').blur(function(){
+	$('#productosajax').fadeOut('fast');
+});
+
+// FUNCION PARA AGREGAR UN PRODUCTO A LA TABLA
+function agregarProducto(codigo, descrp, departamento, varprecio){
+
+	$('#Items').removeClass('HideItem');
+
 	$Item = $('#idItem');
 	$content = $('#Items');
 
 	clone = $Item.clone();
 
 	clone.hide();
+
+	clone.find('#iCodigo').text(codigo);
+	clone.find('#iDescripcion').text(descrp);
+	clone.find('#iPrecio').val(varprecio);
+	clone.find('#iCantidad').val('1');
+
+	clone.addClass(departamento);
+	clone.find('#iPrecio').addClass(departamento);
+	clone.find('#iCantidad').addClass(departamento);
+	clone.find('#iDescuento').addClass(departamento);
+	clone.find('#iImporte').addClass(departamento);
+
 	$content.append(clone);
 
 	clone.fadeIn();
+
+}
+
+// FUNCION DEL BOTON AGREGAR
+$('#btAdd').click(function(){
+	// agregarProducto('BLU01','Blusa con manga','150');
 });
 
+
+// VARIABLE PARA EL CONTENIDO DE LA TABLA DE PRODUCTOS
 var contenido = $('#productosajax');
 
+// FUNCION PARA BUSCAR LOS PRODUCTOS EN LA API Y DIBUJAR LA TABLA CON LOS PRODUCTOS ENCONTRADOS
 $('#iProd').on('input', function(e){
 	e.defaultPrevented = false;
 
-	contenido.html('<table class="tableItem">');
-
 	$.getJSON('http://127.0.0.1:8000/api/productos/' + $('#iProd').val() +'/?format=json',
 		function(data) {
+			contenido.html('');
+
 			$.each(data, function(index, element) {
 				contenido.append(
-					'<tr class="DetailsItem">' + 
-					'<td class="CellItemDetail '+ element['departamento'] +'">' + element['codigo'] 						+'</td>' +					
-					'<td class="CellItemDetail '+ element['departamento'] +'"><a href="">' + element['descripcion'] 	+'</a></td>' +
-					'<td class="CellItemDetail '+ element['departamento'] +'">' + element['precio'] 						+'</td>' +
+					'<tr class="DetailsItemSearch">' + 
+					'<td class="CellItemDetailSearch '+ element['departamento'] +'">' + element['codigo'] +'</td>' +					
+					'<td class="CellItemDetailSearch '+ element['departamento'] +'">'
+						+'<a href="javascript:void(0);" class="aProd"' 	
+						+'onclick="agregarProducto(&quot;' 
+							+ element['codigo'] + '&quot;, &quot;' 
+							+ element['descripcion'] + '&quot;, &quot;'
+							+ element['departamento'] + '&quot;, &quot;'
+							+ element['precio'] + '&quot;); return false;">' + element['descripcion'] +'</a></td>' +
+					'<td class="CellItemDetailSearch '+ element['departamento'] +'">' + element['precio'] +'</td>' +
 					'</tr>'
 					);
-				console.log(element['departamento']);
 			})
+			contenido.hide();
+			contenido.fadeIn('1');	
 		});
-
-	contenido.append('</table>').fadeIn();
 });
 
-	// $.ajax({
-	// 	type: 'GET',
-	// 	url: 'http://127.0.0.1:8000/api/productos/' + $('#iProd').val() +' /?format=json',
-	// 	ContentType: 'application/json; charset=utf-8',
-	// 	success: function(data) {
-	// 		$('#productosajax').html('<h1>' + data.precio +'</H1>')
-	// 		console.log(data);
-	// 	}
-	// })
-	// $('#productosajax').html('<h1>'+ dato +'</h1>')
-// })
 
 //ACCION DE FACTURAR O APARTAR
 $('#BtFacturar').click(function(){
--- productos/admin.py
+++ productos/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Producto
 
-admin.site.register(Producto)
+class ListarProductos(admin.ModelAdmin):
+	list_display = ['codigo','descripcion','precio','departamento','grupo','clasificacion',]
+
+admin.site.register(Producto,ListarProductos)
